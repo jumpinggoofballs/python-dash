5a060fa0294964eabe7c58c3921f84bf79fb42d8
jumpinggoofballs <jumpinggoofballs@users.noreply.github.com>
2024-03-03 20:18:14 +0000

    Basic dash app

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,25 @@
-print('Hello world')
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import investpy
+
+###
+# DASH APP
+###
+
+# Define the app
+# / With title
+appTitle = 'Share price performance against index'
+app = Dash(appTitle)
+
+# Define the layout for the Dash app
+app.layout = html.Div([
+
+    html.H1(children=appTitle, style={'textAlign':'center'}),
+
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
